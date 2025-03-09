7732408fe5ddb20724ed8a8d1480b11b85a06ec5
Arnaszd <163340386+Arnaszd@users.noreply.github.com>
2025-03-10 01:02:26 +0200

    Vercel analytics added

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import StarryBackground from './StarryBackground';
+import { Analytics } from "@vercel/analytics/react"
 
 const Hero = () => {
   // Inicializuoti AOS
